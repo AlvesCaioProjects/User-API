4588fd99fdc517eadb6e853403f01a1746f11ada
Caio Alves <caioanegocios@gmail.com>
2023-12-19 13:30:22 -0300

    User class created

--- src/main/java/com/classcaio/api/domain/User.java
+++ src/main/java/com/classcaio/api/domain/User.java
@@ -1,7 +1,12 @@
 package com.classcaio.api.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -13,7 +18,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    @Column(unique = true)
     private String email;
     private String password;
 }
