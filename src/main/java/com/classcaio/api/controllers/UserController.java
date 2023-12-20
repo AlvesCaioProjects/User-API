fea9a4bba6e40b2d8fe318ce2b3fe4503bd673db
Caio Alves <caioanegocios@gmail.com>
2023-12-20 19:34:55 -0300

    return dto for UserController

--- src/main/java/com/classcaio/api/controllers/UserController.java
+++ src/main/java/com/classcaio/api/controllers/UserController.java
@@ -1,7 +1,8 @@
 package com.classcaio.api.controllers;
 
-import com.classcaio.api.domain.User;
+import com.classcaio.api.domain.dto.UserDTO;
 import com.classcaio.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
