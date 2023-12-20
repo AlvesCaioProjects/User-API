c244828298c06f5e0a899d926dc1638fb8802f4e
Caio Alves <caioanegocios@gmail.com>
2023-12-20 16:34:08 -0300

    exception handling for object not found

--- src/main/java/com/classcaio/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/classcaio/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.classcaio.api.services.impl;
 import com.classcaio.api.domain.User;
 import com.classcaio.api.repositories.UserRepository;
 import com.classcaio.api.services.UserService;
+import com.classcaio.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
