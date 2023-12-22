083edd96485c12ec78f398ac254e1c42ee6b3e1c
Caio Alves <caioanegocios@gmail.com>
2023-12-22 11:11:11 -0300

    Endpoint findAll by User

--- src/main/java/com/classcaio/api/services/UserService.java
+++ src/main/java/com/classcaio/api/services/UserService.java
@@ -2,7 +2,11 @@ package com.classcaio.api.services;
 
 import com.classcaio.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
