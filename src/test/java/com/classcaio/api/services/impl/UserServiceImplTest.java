f58a020482bd92816eee1fd219e47db1d4743ece
Caio Alves <caioanegocios@gmail.com>
2023-12-27 16:21:10 -0300

    Test for UserService class by method FindAll

--- src/test/java/com/classcaio/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/classcaio/api/services/impl/UserServiceImplTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 class UserServiceImplTest {
@@ -21,6 +22,7 @@ class UserServiceImplTest {
     public static final String NAME     = "Caio";
     public static final String EMAIL    = "caio@mail.com";
     public static final String PASSWORD = "123";
+
     @InjectMocks
     private UserServiceImpl service;
 
@@ -67,7 +69,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        Mockito.when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(1, response.size());
+        Assertions.assertEquals(User.class, response.get(0).getClass());
+
+        Assertions.assertEquals(ID, response.get(0).getId());
+        Assertions.assertEquals(NAME, response.get(0).getName());
+        Assertions.assertEquals(EMAIL, response.get(0).getEmail());
+        Assertions.assertEquals(PASSWORD, response.get(0).getPassword());
     }
 
     @Test
